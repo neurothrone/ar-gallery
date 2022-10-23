c4d381bb87f2e2ef0203901d63e919b28f9be284
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-23 05:57:15 +0200

    Initial Commit

---
+++ ARGallery/ARGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARGalleryApp.swift
+//  ARGallery
+//
+//  Created by Zaid Neurothrone on 2022-10-23.
+//
+
+import SwiftUI
+
+@main
+struct ARGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
